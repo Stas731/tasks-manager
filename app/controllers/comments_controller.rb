33cf76ed4419dda458881a3469129db602dca7fc
stanislav <stas.plisko.99@mail.ru>
2024-04-02 14:14:28 +0300

    I set up comments without deleting them

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,43 +1,54 @@
 class CommentsController < ApplicationController
-    before_action :set_task
+  before_action :set_task
+
+  def new
+    @comment = @task.comments.build
+  end
   
-    def create
-      @comment = @task.comments.new(comment_params)
-      if @comment.save
-        redirect_to @task
-      else
-        # обработка ошибок
-      end
-    end
+  def edit
+    @comment = @task.comments.find(params[:id])
+  end
   
-    def edit
-      @comment = @task.comments.find(params[:id])
-    end
-  
-    def update
-      @comment = @task.comments.find(params[:id])
-      if @comment.update(comment_params)
-        redirect_to @task
-      else
-        # обработка ошибок
-      end
-    end
-  
-    def destroy
-      @comment = @task.comments.find(params[:id])
-      @comment.destroy
-      redirect_to @task
-    end
-  
-    private
-  
-    def set_task
-      @task = Task.find(params[:task_id])
-    end
-  
-    def comment_params
-      params.require(:comment).permit(:content)
+  def create
+    @comment = @task.comments.build(comment_params)
+    @comment.user_id = @task.user_id  # Устанавливаем user_id комментария равным user_id задачи
+    if @comment.save
+      redirect_to @task, notice: 'Комментарий успешно добавлен.'
+    else
+      render 'tasks/show'
     end
   end
+
+  def update
+    @comment = @task.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to @task, notice: 'Комментарий успешно обновлен.'
+    else
+      render 'tasks/show'
+    end
+  end
+  def destroy
+   
+    @comment = @task.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @task, notice: 'Комментарий успешно удален.'
+  end
+
+  def show
+    @comment = @task.comments.find(params[:id])
+  end
+  
+  private
+
+  def set_task
+    @task = Task.find(params[:task_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
+
+
   
   
